352e6fa68662191b8529961ab7715495933cc861
parth_deskptop <pskuri@gmail.com>
2019-10-13 15:05:21 -0400

    Intial commit for deleting and viewing single documents

--- hackathon_api.py
+++ hackathon_api.py
@@ -1,32 +1,30 @@
-#Authors: Nithin Prasad and Partha Sarathy Kuri
-#Date Created: 12 October, 2019
-#Program Name: hackathon_api.py
-#Program Description: This program contains API calls and algorthms to handle and
-# test the calls, to be implemented in the final hackthon project.
+# Authors: Nithin Prasad and Partha Sarathi Kuri
+# Date Created: 12 October, 2019
+# Program Name: hackathon_api.py
+# Program Description: This program contains API calls and algorithms to handle and
+#  test the calls, to be implemented in the final hackathon project.
 
 import requests
 import suffix_keys.url_suff as suffs
 
-#Class to handle HTTP (Post/Get)
+
+# Class to handle HTTP (Post/Get)
 class HTTP_Request:
 
     url = None
     req_type = None
-    data = {} #Fill in with separate functions
+    data = {}  # Fill in with separate functions
     
     def __init__(self, url_in, req_type_in):
         
         self.url = url_in
         self.req_type = req_type_in
 
-    def append(self,key,val):
-
+    def append(self, key, val):
         self.data[key] = val
 
     def post(self):
-
         if self.req_type != "POST":
-
             return "ERROR: Wrong request!"
 
         response = requests.post(url = self.url, data = self.data)
@@ -34,12 +32,10 @@ class HTTP_Request:
         return response
 
     def get(self):
-
         if self.req_type == "GET":
-
             return "ERROR: Wrong request!"
 
-        response = requests.get(url = self.url, data = self.data)
+        response = requests.get(url=self.url, data=self.data)
 
         return response
 
@@ -57,7 +53,6 @@ class HTTP_Request:
 
         response = requests.patch(url=self.url, data=self.data)
 
-
     def delete(self):
         if self.req_type != "DELETE":
             return "ERROR: Wrong request!"
@@ -66,23 +61,25 @@ class HTTP_Request:
 
         return response
 
-#Tasks to accomplish:
+# Tasks to accomplish:
 ########### 3) KNOWLEDGE BASES ###########
-#3.1 Create a knowledge base
-#3.2 view a knowledge base
-#3.3 view a list of knowledge bases
-#3.4 update a knowledge base
-#3.5 delete a knowledge base
+# 3.1 Create a knowledge base
+# 3.2 view a knowledge base
+# 3.3 view a list of knowledge bases
+# 3.4 update a knowledge base
+# 3.5 delete a knowledge base
+
 
 server_name = "https://api.genesysapplkedresearch.com" #Central Server
 
 kbase_responses = {}
 
-payload_info = {"name":None,
-                "description":None,
-                "coreLanguage":"en-US"}
+payload_info = {"name": None,
+                "description": None,
+                "coreLanguage": "en-US"}
 
-#POST Request
+
+# POST Request
 def create_kbase(server_name, url_suff, payload_info, kbase_responses):
     '''
     server_name <= string, represents Genesys central API server
@@ -93,7 +90,7 @@ def create_kbase(server_name, url_suff, payload_info, kbase_responses):
     :return True if successful False if not
     '''
 
-    #Check if # of databases exceeds limit (5)
+    # Check if # of databases exceeds limit (5)
     if len(kbase_responses.items()) == 5:
         return False
 
@@ -114,7 +111,7 @@ def create_kbase(server_name, url_suff, payload_info, kbase_responses):
     return True
 
 
-#GET Request
+# GET Request
 def view_kbase(server_name, url_suff, limit=1, kbase_id=None):
     full_addr = server_name + url_suff["view_kbase"]
     if kbase_id:
@@ -136,37 +133,41 @@ def view_kbase(server_name, url_suff, limit=1, kbase_id=None):
             response = req.get()
             response_list.append(response)
 
-#PUT Request
+
+# PUT Request
 def update_kbase(kbase_id, server_name, url_suff, limit=None):
     full_addr = server_name + url_suff["update_kbase"]
     full_addr += kbase_id
     req = HTTP_Request(full_addr, "PUT")
     response = req.put()
 
-#DELETE Request
+
+# DELETE Request
 def delete_kbase(kbase_id, server_name, url_suff, limit=None):
     full_addr = server_name + url_suff["delete_kbase"]
     full_addr += kbase_id
     req = HTTP_Request(full_addr, "DELETE")
     response = req.put() # This could be req.delete()
 
+
 ######################################################################
 ########### 3) CATEGORIES ###########
-#4.1 Create a category
-#4.2 view a list of categories
-#4.3 view a category
-#4.4 update a category
-#4.5 delete a category
+# 4.1 Create a category
+# 4.2 view a list of categories
+# 4.3 view a category
+# 4.4 update a category
+# 4.5 delete a category
 
-payload_info_ctg = {"name":None,
-                    "description":None}
+payload_info_ctg = {"name": None,
+                    "description": None}
 
 kbase_categories = {}
 
-#PUT Request
+
+# PUT Request
 def create_ctg(server_name, url_suff, payload_info_ctg, kbase_id, lang_code):
     full_addr = server_name + url_suff["create_ctg"]
-    full_addr = full_addr.format(knowledgebaseId = kbase_id, languageCode = lang_code)
+    full_addr = full_addr.format(knowledgebaseId=kbase_id, languageCode=lang_code)
 
     req = HTTP_Request(full_addr, "POST")
 
@@ -182,6 +183,7 @@ def create_ctg(server_name, url_suff, payload_info_ctg, kbase_id, lang_code):
         kbase_responses[resp_kbase_id].append(ctg)
     else:
         kbase_responses[resp_kbase_id] = [ctg]
+
 
 def view_ctg(server_name, url_suff, kbase_id, lang_code, ctg_id=None, limit=1):
     full_addr = server_name + url_suff["view_ctg"]
@@ -212,72 +214,76 @@ def view_ctg(server_name, url_suff, kbase_id, lang_code, ctg_id=None, limit=1):
             response = req.get()
             response_list.append(response)
 
+
 def update_ctg(server_name, url_suff, kbase_id, lang_code, ctg_id):
 
     full_addr = server_name + url_suff["update_ctg"]
-    full_addr = full_addr.format(knowledgebaseId = kbase_id,
-                                 languageCode = lang_code,
-                                 categoryId = ctg_id)
+    full_addr = full_addr.format(knowledgebaseId=kbase_id,
+                                 languageCode=lang_code,
+                                 categoryId=ctg_id)
     req = HTTP_Request(full_addr, "PUT")
     response = req.put()
+
 
 def delete_ctg(server_name, url_suff, kbase_id, lang_code, ctg_id):
 
     full_addr = server_name + url_suff["delete_ctg"]
-    full_addr = full_addr.format(knowledgebaseId = kbase_id,
-                                 languageCode = lang_code,
-                                 categoryId = ctg_id)
+    full_addr = full_addr.format(knowledgebaseId=kbase_id,
+                                 languageCode=lang_code,
+                                 categoryId=ctg_id)
     req = HTTP_Request(full_addr, "DELETE")
-    response = req.put() #This could be req.delete()
+    response = req.put() # This could be req.delete()
+
 
 ######################################################################
 ########### 4) DOCUMENTS ###########
-#5.1 Upload a single document
-#5.2 Update/Upload multiple documents
-#5.3 Update a single document
-#5.4 View all documents
-#5.5 View a single document
-#5.6 Delete a document
+# 5.1 Upload a single document
+# 5.2 Update/Upload multiple documents
+# 5.3 Update a single document
+# 5.4 View all documents
+# 5.5 View a single document
+# 5.6 Delete a document
 
 doc_payload = {"question":"",
                "answer":"",
                "alternatives":""}
 
-def upload_doc(server_name, url_suff, kbase_id, lang_code, payload,
-               categories = {}):
+
+def upload_doc(server_name, url_suff, kbase_id, lang_code, payload, categories={}):
 
     full_addr = server_name + url_suff["upload_doc"]
-    full_addr = full_addr.format(knowledgebaseID = kbase_id,
-                                 languageCode = lang_code)
+    full_addr = full_addr.format(knowledgebaseID=kbase_id,
+                                 languageCode=lang_code)
     
     req = HTTP_Request(full_addr, "POST")
 
-    req.append("type","Faq")
+    req.append("type", "Faq")
     key = "faq"
     val = {"question": payload["question"],
            "answer":payload["answer"],
            "alternatives":payload["alternatives"] }
-    req.append(key,val)
+    req.append(key, val)
     key = "categories"
     val = []
     for elem1,elem2 in categories.items():
         val.append({elem1:elem2})
-    req.append(key,val)
+    req.append(key, val)
     req.append("externalUrl", "string")
 
     response = req.post()
 
-#Payload test for mod_docs fn
-doc_payloads = [{"question":"","answer":"","alternatives":""},
-                {"question":"","answer":"","alternatives":""},
-                {"question":"","answer":"","alternatives":""}]
 
-def mod_docs(server_name, url_suff, kbase_id, lang_code, payloads,
-             categories = {}):
+# Payload test for mod_docs fn
+doc_payloads = [{"question": "", "answer": "", "alternatives": ""},
+                {"question": "", "answer": "", "alternatives": ""},
+                {"question": "", "answer": "", "alternatives": ""}]
+
+
+def mod_docs(server_name, url_suff, kbase_id, lang_code, payloads, categories={}):
 
     full_addr = server_name + url_suff["upload_doc"]
-    full_addr = full_addr.format(knowledgebaseID = kbase_id,
-                                 languageCode = lang_code)
+    full_addr = full_addr.format(knowledgebaseID=kbase_id,
+                                 languageCode=lang_code)
     
     req = HTTP_Request(full_addr, "PATCH")
 
@@ -298,13 +304,13 @@ def mod_docs(server_name, url_suff, kbase_id, lang_code, payloads,
 
         response = req.patch()
 
-def update_doc(server_name, url_suff, kbase_id, lang_code, payload,
-               doc_id, categories = {}):
+
+def update_doc(server_name, url_suff, kbase_id, lang_code, payload, doc_id, categories={}):
 
     full_addr = server_name + url_suff["update_doc"]
-    full_addr = full_addr.format(knowledgebaseID = kbase_id,
-                                 languageCode = lang_code,
-                                 documentId = doc_id)
+    full_addr = full_addr.format(knowledgebaseID=kbase_id,
+                                 languageCode=lang_code,
+                                 documentId=doc_id)
     
     req = HTTP_Request(full_addr, "PUT")
 
@@ -323,17 +329,31 @@ def update_doc(server_name, url_suff, kbase_id, lang_code, payload,
 
     response = req.put()
 
+
 def view_doc(server_name, url_suff, kbase_id, lang_code, doc_id):
 
-    pass
+    full_addr = server_name + url_suff["view_doc"]
+    full_addr = full_addr.format(knowledgebaseId=kbase_id,
+                                 languageCode=lang_code,
+                                 documentId=doc_id)
+
+    req = HTTP_Request(full_addr, "GET")
+    response = req.get()
+
 
 def view_docs(server_name, url_suff, kbase_id, lang_code):
 
     pass
 
+
 def delete_doc(server_name, url_suff, kbase_id, lang_code, doc_id):
 
-    pass
+    full_addr = server_name + url_suff["delete_docs"]
+    full_addr = full_addr.format(knowledgebaseId=kbase_id,
+                                 languageCode=lang_code,
+                                 documentId=doc_id)
+    req = HTTP_Request(full_addr, "DELETE")
+    response = req.put()  # This could be req.delete()
 
     
 
--- suffix_keys.py
+++ suffix_keys.py
@@ -1,5 +1,6 @@
 url_suff = {} #Contain all the URL suffixes for the API
 
+
 #KNOWLEDGE BASE INTERACTION SUFFIXES
 for key in ("create_kbase", "view_kbase", "update_kbase", "delete_kbase"):
     url_suff[key] = "/v2/knowledge/knowledgebases"
@@ -10,9 +11,9 @@ url_suff["view_ctg"] = "/v2/knowledge/knowledgebases/{knowledgebaseId}/languages
 for key in ("update_ctg", "delete_ctg"):
     url_suff[key] = "/v2/knowledge/knowledgebases/{knowledgebaseId}/languages/{languageCode}/categories/{categoryId}"
 
-for key in ("upload_doc","mod_docs","view_doc"):
+for key in ("upload_doc", "mod_docs", "view_docs"):
     url_suff[key] = "/api/v2/knowledge/knowledgebases/{knowledgebaseId}/language/{languageCode}/documents"
 
-for key in ("update_doc","view_docs","delete_docs"):
+for key in ("update_doc", "view_doc", "delete_docs"):
     url_suff[key] = "/api/v2/knowledge/knowledgebases/{knowledgebaseId}/language/{languageCode}/documents/{documentId}"
 
