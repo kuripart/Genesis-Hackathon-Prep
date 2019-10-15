6c06aa8afb0bf2d17b40e4d474b5048ffb4779c2
parth_desktop <pskuri@gmail.com>
2019-10-14 21:32:56 -0400

    correcting package name

--- hackathon_04/manage.py
+++ hackathon_04/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon_03.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'hackathon_04.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
