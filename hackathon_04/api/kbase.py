41bfda1c05d969f226d8723b83ea9325aba7eeec
Nithin Prasad <nithin.ganeshprasad@mail.utoronto.ca>
2019-10-19 18:30:17 -0400

    Committing testing_api and other changed files (newly added though)

---
+++ hackathon_04/api/kbase.py
@@ -0,0 +1,126 @@
+from HTTPRequest import HTTPRequest
+from suffix_keys import url_suff as suff
+<<<<<<< HEAD
+import suffix_keys
+import json
+=======
+>>>>>>> fd1799c14c2e68c12f1b4bc512c9aab808d37bd6
+
+
+# 3) KNOWLEDGE BASES
+# 3.1 Create a knowledge base
+# 3.2 view a knowledge base
+# 3.3 view a list of knowledge bases
+# 3.4 update a knowledge base
+# 3.5 delete a knowledge base
+
+# POST Request
+def create_kbase(server_name, url_suff, payload_info, org_id, token,kbase_responses={}):
+
+    # Check if # of databases exceeds limit (5)
+    if len(kbase_responses.items()) == 5:
+        return {},
+
+    full_addr = server_name + url_suff["create_kbase"]
+
+    req = HTTPRequest(full_addr, "POST")
+
+    for key in payload_info.keys():
+        req.payload_append(key, payload_info[key])
+
+    #'Content-Type': "application/json",
+    #'organizationid': "3ae6bd8b-23b6-47c7-a9a0-8dc56833ca18",
+    #'token': "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdJZCI6IjNhZTZiZDhiLTIzYjYtNDdjNy1hOWEwLThkYzU2ODMzY2ExOCIsImV4cCI6MTU3MTUxNjYyOCwiaWF0IjoxNTcxNTEzMDI4fQ.S1mQ4sdeSaPhTCwUodVOb6QW3yOzTnQKOunSU732SSI",
+    #'cache-control': "no-cache",
+    req.add_header("Content-Type","application/json")
+    req.add_header("organizationid",org_id)
+    req.add_header("token", token)
+    req.add_header("cache-control","no-cache")
+
+    response = req.post()
+    print(response)
+    print("response: ",response.json())
+
+    # resp_id = response.json().get("id")
+    # kbase_responses['status_code'] = response.status_code
+    # if resp_id:
+    #     kbase_responses[resp_id] = (response["dateCreated"],
+    #                                 response["dateModified"],
+    #                                 response["selfUri"])
+    # else:
+    #     return kbase_responses
+    #
+    # return kbase_responses
+
+
+# GET Request
+def view_kbase(server_name, url_suff, org_id, token, limit=1, kbase_id=None):
+    full_addr = server_name + url_suff["view_kbase"]
+    response_list = []
+    if kbase_id:
+        full_addr += kbase_id
+        req = HTTPRequest(full_addr, "GET")
+        req.add_header("Content-Type", "application/json")
+        req.add_header("organizationid", org_id)
+        req.add_header("token", token)
+        req.add_header("cache-control", "no-cache")
+        response = req.get()
+        response_dict = response.json()
+        if response_dict.status_code == 200:
+            response_list.append(response_dict)
+    else:
+        full_addr += '?limit={0}'.format(limit)
+        req = HTTPRequest(full_addr, "GET")
+        req.add_header("Content-Type", "application/json")
+        req.add_header("organizationid", org_id)
+        req.add_header("token", token)
+        req.add_header("cache-control", "no-cache")
+        response = req.get()
+        print("Response:",response.json())
+        response_dict = response.json()
+        if response.status_code == 200:
+
+            response_list = response_dict["entities"]
+            print("Resp List",response_list)
+
+            # while response_dict.get("nextUri") != None:
+            #     print("in loop")
+            #     full_addr = server_name + url_suff["view_kbase"]
+            #     full_addr += '?limit={0}'.format(limit)
+            #     full_addr += response_dict.get("nextUri")
+            #     req = HTTPRequest(full_addr, "GET")
+            #     response = req.get()
+            #     response_dict = response.json()
+            #     if response.status_code == 200:
+            #         response_list.append(response_dict)
+            ret_list = []
+            for elem in response_list:
+                ret_list.append(elem['id'])
+            return ret_list
+
+    return response_list
+
+
+# PUT Request
+def update_kbase(server_name, url_suff, payload_info, kbase_id):
+    full_addr = server_name + url_suff["update_kbase"]
+    full_addr += kbase_id
+    req = HTTPRequest(full_addr, "PUT")
+    for key in payload_info.keys():
+        req.payload_append(key, payload_info[key])
+    response = req.put()
+    response_result = response.json()
+    response_result['status_code'] = response.status_code
+    return response_result
+
+
+# DELETE Request
+def delete_kbase(server_name, url_suff, kbase_id):
+    full_addr = server_name + url_suff["delete_kbase"]
+    full_addr += kbase_id
+    req = HTTPRequest(full_addr, "DELETE")
+    response = req.delete()
+    response_result = response.json()
+    response_result['status_code'] = response.status_code
+    return response_result
+
---
+++ hackathon_04/api/testing_api.py
@@ -0,0 +1,75 @@
+from HTTPRequest import HTTPRequest
+from suffix_keys import url_suff as suff
+<<<<<<< HEAD
+
+import requests
+import json
+import categories
+import documents
+import kbase
+import training
+import search
+
+if __name__ == "__main__":
+
+    server_name = "https://api.genesysappliedresearch.com"
+    org_id = "3ae6bd8b-23b6-47c7-a9a0-8dc56833ca18"
+    client_secret = "7ab90651-f1c6-4756-a980-3813bf682198"
+    token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJvcmdJZCI6IjNhZTZiZDhiLTIzYjYtNDdjNy1hOWEwLThkYzU2ODMzY2ExOCIsImV4cCI6MTU3MTUyNjUxMiwiaWF0IjoxNTcxNTIyOTEyfQ.HVelm58b8HA4yx1PASrO84g8wAiQRCWtv73gYz7WtOQ"
+
+    #payload_info = {"name": "Sample KBase",
+    #                "description": "Testing our preliminary kbase!",
+     #               "coreLanguage": "en-US"}
+
+    #kbase_responses = {}
+
+    print("-----------------------------------------------------------")
+    #kbase.create_kbase(server_name,suff,payload_info,org_id,token,kbase_responses)
+
+    #VIEW Knowledge-Base
+    print("-----------------------------------------------------------")
+    resp_list = kbase.view_kbase(server_name, suff, org_id, token, limit=5, kbase_id=None)
+    print(resp_list)
+    #List: ['891c0775-18d5-4f4c-9222-314a7ff650ac', '254a5aae-225f-412c-98c2-b1f5dfd6c6b9', 'fd41a9b6-442c-44d5-b6b7-0317d6add350', '68e2b7b8-b3cd-4af0-b21b-f699ea9d3a00', '6bda054f-e54d-4232-96e5-34c59bf2b0f5']
+
+    #Load 5 Docs
+    # questions = ["What is the state dessert of Arizona?",
+    # "What is the national spirit of America?",
+    # "In 1963, which state sold the first fried dill pickle?",
+    # "Name the state which is the birthplace of Cheeseburger?",
+    # "What is the national dish of Germany?"]
+    #
+    # answers = ["Lane Cake","Bourbon","Arkansas","Colorado","Pot Roast"]
+    #
+    kbase1_id = '891c0775-18d5-4f4c-9222-314a7ff650ac'
+    lang_code = "en-US"
+    #
+    print("-----------------------------------------------------------")
+    # for i in range(1,5):
+    #     payload = {"question":questions[i],"answer":answers[i]}
+    #     documents.upload_doc(server_name, suff, kbase1_id, lang_code, payload, org_id, token, categories={})
+
+    #TRAIN!
+    print("-----------------------------------------------------------")
+    #training_id = training.train_model(server_name, suff, kbase1_id, lang_code, org_id, token)
+    #print("Training ID: ",training_id)
+
+    #Check Training!
+    print("-----------------------------------------------------------")
+    training_id = "12143149-114c-40f5-8b09-6f7b7df88530"
+    status_code = training.view_trained_model(server_name, suff, kbase1_id, lang_code,training_id, org_id, token)
+    print("status code: ", status_code)
+
+    #Try search query!
+    print("-----------------------------------------------------------")
+    payload = {"query":"What is the national spirit of Canada?"}
+    results = search.search(server_name, suff, kbase1_id, payload, org_id, token)
+    print(results)
+    #print(kbase_responses)
+=======
+import kbase
+
+if __name__ == '__main__':
+    q = HTTPRequest('A', 'B')
+    kbase.create_kbase('a', 's', 'a')
+>>>>>>> fd1799c14c2e68c12f1b4bc512c9aab808d37bd6
